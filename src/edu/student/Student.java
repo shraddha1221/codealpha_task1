38f1fc5f16dd9380c56a46bd3b7e758d3fc30771
Shraddha Karma <shraddhakarma2112@gmail.com>
2024-07-18 18:52:21 +0530

    Update Student.java

--- src/edu/student/Student.java
+++ src/edu/student/Student.java
@@ -5,24 +5,20 @@ public class Student {
 	    private int rollNumber;
 	    private int[] grades;
 	    
-	    // Constructor
 	    public Student(String name, int rollNumber, int numSubjects) {
 	        this.name = name;
 	        this.rollNumber = rollNumber;
 	        this.grades = new int[numSubjects];
 	    }
-	    
-	    // Getter for name
+	
 	    public String getName() {
 	        return name;
 	    }
 	    
-	    // Getter for roll number
 	    public int getRollNumber() {
 	        return rollNumber;
 	    }
 	    
-	    // Getter for grades array
 	    public int[] getGrades() {
 	        return grades;
 	    }
